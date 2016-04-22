3e0b90b6d7d93b28f45006230a3d707e5b7dd1c1
Valerie Burzynski <valerie@bendyworks.com>
2016-04-22 16:54:32 -0500

    First Commit

---
+++ Gruntfile.js
@@ -0,0 +1,162 @@
+/*global module:false*/
+module.exports = function(grunt) {
+
+  // Project configuration.
+  grunt.initConfig({
+    // Metadata.
+    dirTmp: '.tmp/',
+    dirRelease: 'build/release/',
+    dirDebug: 'build/debug/',
+    pkg: grunt.file.readJSON('package.json'),
+    banner: '/*! <%= pkg.title || pkg.name %> - v<%= pkg.version %> - ' +
+    '<%= grunt.template.today("yyyy-mm-dd") %>\n' +
+    '<%= pkg.homepage ? "* " + pkg.homepage + "\\n" : "" %>' +
+    '* Copyright (c) <%= grunt.template.today("yyyy") %> <%= pkg.author.name %>;' +
+    ' Licensed <%= _.pluck(pkg.licenses, "type").join(", ") %> */\n',
+
+    // Task configuration.
+
+
+    uglify: {
+      options: {
+        banner: '<%= banner %>'
+      },
+      prod: {
+        files: {
+          '<%= dirRelease %>app.js': '<%= dirTmp %>app.js'
+        }
+      },
+      dist: {
+        src: '<%= concat.dist.dest %>',
+        dest: 'dist/<%= pkg.name %>.min.js'
+      }
+    },
+
+    browserify: {
+      dev: {
+        files: {
+          '<%= dirDebug %>app.js': 'src/main.js'
+        },
+        transform: ["browserify-shim"],
+        options: {
+          bundleOptions : {
+              debug: true
+          }
+        }
+      },
+      prod: {
+        files: {
+          '<%= dirTmp %>app.js': 'src/main.js'
+        },
+        transform: ["browserify-shim"],
+        options: {}
+      }
+    },
+
+    watch: {
+      // watch lib and src files, then browserify them for dev
+      dev: {
+        files: [
+          './src/**/*.js'
+        ],
+        tasks: [
+          'browserify:dev'
+        ],
+        options: {
+          livereload: true,
+          spawn: false,
+          atBegin: true
+        }
+      },
+      gruntfile: {
+        files: '<%= jshint.gruntfile.src %>',
+        tasks: ['jshint:gruntfile']
+      },
+      lib_test: {
+        files: '<%= jshint.lib_test.src %>',
+        tasks: ['jshint:lib_test', 'qunit']
+      }
+    },
+
+    connect: {
+      dev: {
+        options: {
+          port: 3000,
+          base: '.'
+        }
+      }
+    },
+
+    concat: {
+      options: {
+        banner: '<%= banner %>',
+        stripBanners: true
+      },
+      dist: {
+        src: ['lib/<%= pkg.name %>.js'],
+        dest: 'dist/<%= pkg.name %>.js'
+      }
+    },
+
+    jshint: {
+      options: {
+        curly: true,
+        eqeqeq: true,
+        immed: true,
+        latedef: true,
+        newcap: true,
+        noarg: true,
+        sub: true,
+        undef: true,
+        unused: true,
+        boss: true,
+        eqnull: true,
+        browser: true,
+        globals: {
+          jQuery: true
+        }
+      },
+      gruntfile: {
+        src: 'Gruntfile.js'
+      },
+      lib_test: {
+        src: ['lib/**/*.js', 'test/**/*.js']
+      }
+    },
+
+    qunit: {
+      files: ['test/**/*.html']
+    }
+  });
+
+  // These plugins provide necessary tasks.
+  grunt.loadNpmTasks('grunt-contrib-concat');
+  grunt.loadNpmTasks('grunt-contrib-connect');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-qunit');
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-browserify');
+  grunt.loadNpmTasks('browserify-shim');
+
+  // Default task.
+  // grunt.registerTask('default', ['jshint', 'qunit', 'concat', 'uglify']);
+
+  grunt.registerTask('debug', [
+    'browserify:dev'
+  ]);
+
+  grunt.registerTask('server', [
+    'connect:dev',
+    'watch:dev'
+  ]);
+
+  grunt.registerTask('release', [
+    'browserify:prod',
+    'uglify:prod'
+  ]);
+
+  grunt.registerTask('default', [
+    'debug'
+  ]);
+};
---
+++ src/main.js
@@ -0,0 +1,15 @@
+var $ = require('jquery'),
+    _ = require('lodash');
+
+window.PIXI   = require('../bower_components/phaser/build/custom/pixi.js');
+window.p2     = require('../bower_components/phaser/build/custom/p2.js');
+window.Phaser = require('../bower_components/phaser/build/custom/phaser-split.js');
+
+var game = new Phaser.Game(800, 600, Phaser.CANVAS, 'phaser-example', { create: create });
+
+function create() {
+    var text = "- phaser -\n with a sprinkle of \n pixi dust!!!!!";
+    var style = { font: "65px Arial", fill: "#ff0044", align: "center" };
+    var t = game.add.text(game.world.centerX-300, 0, text, style);
+    console.log('hello world');
+}
